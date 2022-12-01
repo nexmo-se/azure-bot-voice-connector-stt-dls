04826f783a6fb53965f9c1882020455e2c542a12
Tony Chan <tony.chan@vonage.com>
2022-11-30 23:20:53 -0800

    azure-tts

--- azure-bot-voice-connector-stt-dls.js
+++ azure-bot-voice-connector-stt-dls.js
@@ -135,10 +135,141 @@ app.ws('/socket', async (ws, req) => {
 
   const msaConnector = new msaSpeechSdk.DialogServiceConnector(msaBotConfig, msaAudioConfig);
 
+  //--- TTS play to client over WebSocket ---
+
+  async function playAudio(audioPayload) {
+
+    // TO DO: add flag and wait timers to prevent simultaneous playbacks
+
+    // const audioPayload = new Uint8Array(payload).slice(0);
+    
+    try {
+      
+      const timerIds = app.get(`playtimers_${originalUuid}`) || [];
+      
+      for (const timerId of timerIds) {
+        clearTimeout(timerId);
+      };
+
+      console.log(">>> audio payload length:", audioPayload.length);
+
+      if ( audioPayload.length > 0 ) {
+
+        console.log(">>> Playback in progress for websocket associated to original uuid:", originalUuid);
+        
+        // Sending Bot audio response to caller via websocket
+        
+        const frames = audioPayload.length / 640;
+        // console.log({frames});
+        let pos = 0;
+        const timerIds = [];
+        
+        for (let i = 0; i < frames + 1; i++) {
+          const newpos = pos + 640;
+          const data = audioPayload.slice(pos, newpos);
+          
+          timerIds.push(setTimeout(function () {
+            if (ws.readyState === 1) {  // Send data only if websocket is up
+              ws.send(data);
+            }
+          }, i * 20))  // Send a frame every 20 ms
+          
+          pos = newpos;
+
+        }
+
+        app.set(`playtimers_${originalUuid}`, timerIds);
+
+      }
+    } catch (e) {
+      
+      console.log("WebSocket error:", e);
+    
+    }
+
+  }  
+
+  //--- Azure TTS setup ---
+
+  const msaTtsSpeechConfig = msaSpeechSdk.SpeechConfig.fromSubscription(msaSpeechServiceKey, msaRegion);
+  msaTtsSpeechConfig.speechSynthesisLanguage = languageCode;
+  msaTtsSpeechConfig.speechSynthesisOutputFormat = 'Raw16Khz16BitMonoPcm';
+  msaTtsSpeechConfig.speechSynthesisVoiceName = 'en-US-JennyNeural'; // temporary hard coding - TBD: to be set by the bot reply
+
+  // class PushAudioOutputStreamToClientCallback extends msaSpeechSdk.PushAudioOutputStreamCallback {
+  //   constructor(cb) {
+  //       super();
+  //       this.buffer;
+  //       this.length = 0;
+  //       this.isClosed = false;
+  //       this.cb = cb;
+  //   }
+
+  //   write(dataBuffer) {
+  //     try {
+  //       // console.log("writing to buffer...")
+  //       if (this.isClosed) {
+  //         console.log("you can't write while the buffer is closed");
+  //         throw new InvalidOperationError("PushAudioOutputStreamCallback already closed");
+  //       }
+  //       if (!this.buffer) {
+  //         this.buffer = new Uint8Array(dataBuffer);
+  //       } else {
+  //         const temp = new Uint8Array(this.length + dataBuffer.byteLength);
+  //         // console.log({buflen: this.length, data: dataBuffer});
+  //         temp.set(this.buffer, 0);
+  //         temp.set(dataBuffer, this.length);
+  //         // console.log(buf2hex(dataBuffer));
+  //         this.buffer = temp;
+  //       }
+  //       this.length += dataBuffer.byteLength;
+  //       // console.log("wrote to buffer:");
+  //       // console.log(this.length);
+  //       // console.log(dataBuffer);
+  //     } catch(e) {
+  //       console.log("failed to write");
+  //       console.log(e);
+  //     }
+      
+  //   }
+
+  //   close() {
+  //       if (this.isClosed) {
+  //           throw new InvalidOperationError("PushAudioOutputStreamCallback already closed");
+  //       }
+  //       this.isClosed = true;
+  //       console.log("completed buffer:");
+  //       // console.log(buf2hex(this.buffer));
+  //       b1 = new Uint8Array(this.buffer);
+  //       this.cb(this, this.buffer, this.length);
+  //       console.log("the buffer was closed");
+  //   }
+
+  //   // clearBuffer() {
+  //   //   this.buffer = undefined;
+  //   //   this.length = 0;
+  //   //   console.log("the buffer was cleared");
+  //   // }
+  // }
+
+  //---
+
+  // const replyAudioCallback = async (pushAudio, buffer, length) => {
+  //   console.log(`the full buffer is ${length} bytes long`);
+  //   // console.log(`the full buffer is ${length} bytes long, here it is:`);
+  //   // console.log(buffer);
+  //   // pushAudio.clearBuffer();
+
+  //   // send buffer payload over Vonage WebSocket to the client
+  //   await playAudio(buffer);
+
+  // };
+
+  const msaSpeechSynthesizer = new msaSpeechSdk.SpeechSynthesizer(msaTtsSpeechConfig);
+
   //--- Azure Bot Framework operation ---
 
   msaConnector.activityReceived = (sender, event) => {
-    // console.log(`Activity received - Event: ${event} - Has audio: ${event.hasaudio} - Activity: ${event.activity} - Sender: ${sender}`);
     
     console.log("\n>>>>> Activity received >>>>>");
 
@@ -148,6 +279,18 @@ app.ws('/socket', async (ws, req) => {
     console.log('Bot reply:', botReply);
 
     returnReply(latestTranscript, botReply, languageCode, originalUuid, webhookUrl, customQueryParams) ;
+
+    msaSpeechSynthesizer.speakTextAsync(botReply, (result) => {
+      if (result.reason === msaSpeechSdk.ResultReason.SynthesizingAudioCompleted) {
+        console.log({result});
+        playAudio(new Uint8Array(result.privAudioData));
+      } else {
+        console.log("speech synth ended with an error reason");
+      }
+    }, (e) => {
+        console.error(e);
+    });
+
   };
 
   msaConnector.recognizing = (sender, event) => {
@@ -160,37 +303,10 @@ app.ws('/socket', async (ws, req) => {
       console.log("\nRecognized - Result:");
       console.log(event);
 
-      // tests - temporarily commented out      
-      // const transcript = event.result.text;
-      // console.log("Final transcript:", transcript);
-
-      // if (transcript != '') {
-      //   sendTranscript(transcript, languageCode, originalUuid, webhookUrl, customQueryParams) ;
-      // } else {
-      //   console.log("Empty transcript!")
-      // }
-
-
-      //execute a new listen -- this will not work because the session stopped
-      msaConnector.listenOnceAsync((event) => {
-        console.log("\nAzure Bot listen once async #1 ...");
-      },
-        event => {
-          console.log(JSON.stringify(event));
-      });
-
     }
     else if (event.result.reason == msaSpeechSdk.ResultReason.NoMatch) {
       
       console.log("\nNo speech detected");
-
-      //execute a new listen -- this will not work because the session stopped
-      msaConnector.listenOnceAsync((event) => {
-        console.log("\nAzure Bot listen once async #2 ...");
-      },
-        event => {
-          console.log(JSON.stringify(event));
-      });
     
     }
   };
@@ -232,7 +348,6 @@ app.ws('/socket', async (ws, req) => {
 
   //--- Azure STT operation ---
 
-  // commented out, too verbose
   // msaSpeechRecognizer.recognizing = (s, e) => {
   //   console.log(`Intermediate transcript: ${e.result.text}`);
   // };
@@ -251,18 +366,7 @@ app.ws('/socket', async (ws, req) => {
 
         console.log('inputActivity:', inputActivity);
 
-        // does not work        
-        // await msaConnector.sendActivityAsync( (inputActivity) => {
-        //   console.log("Send input activity ...");
-        // },
-        // err => {
-        //   console.log("Send input activity error:", err);
-        // });
-
         sendRequestToBot(inputActivity);
-
-        // do webhook call elsewhere; request/reply info
-        // sendTranscript(transcript, languageCode, originalUuid, webhookUrl, customQueryParams) ;
       
       } else {
         
@@ -356,13 +460,15 @@ app.ws('/socket', async (ws, req) => {
 
     msaSpeechRecognizer.stopContinuousRecognitionAsync();
 
+    msaSpeechSynthesizer.close();
+
     console.log("WebSocket closed");
     
   });
 
 });
 
-//-------------------- for Neru --------
+//------------------ for Neru ----------------------
 
 app.get('/_/health', async (req, res) => {
     res.sendStatus(200);
@@ -372,6 +478,6 @@ app.get('/_/health', async (req, res) => {
 
 const port = process.env.NERU_APP_PORT || process.env.PORT || 6000;
 
-app.listen(port, () => console.log(`Azure Speech-to-Text - connector server code running on port ${port}.`));
+app.listen(port, () => console.log(`Azure Bot Framework - Voice connector server running on port ${port}.`));
 
 //------------
